1e2395eb11aa457cba3f6daa30f9afbc4b60d3e9
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2021-10-10 18:30:35 +0900

    Load style.json from stdin (#19)

--- src/commands/convert.ts
+++ src/commands/convert.ts
@@ -1,32 +1,10 @@
 import path from 'path'
 import fs from 'fs'
 import YAML from 'js-yaml'
+import readline from 'readline'
 
-export function convert(source: string, destination: string) {
-  let sourcePath = path.resolve(process.cwd(), source)
-
-  // The `source` is absolute path.
-  if (source.match(/^\//)) {
-    sourcePath = source
-  }
-
-  if (! fs.existsSync(sourcePath)) {
-    throw `${sourcePath}: No such file or directory`
-  }
-
-  let destinationPath = ""
-
-  if (destination) {
-    if (destination.match(/^\//)) {
-      destinationPath = destination
-    } else {
-      destinationPath = path.resolve(process.cwd(), destination)
-    }
-  } else {
-    destinationPath = path.join(path.dirname(sourcePath), `${path.basename(source, '.json')}.yml`)
-  }
-
-  const style = JSON.parse(fs.readFileSync(sourcePath, 'utf-8'))
+// TODO: Type of style should be loaded from maplibre or mapbox style spec.
+const writeYaml = (destinationPath: string, style: any) => {
   const layers = []
 
   for (let i = 0; i < style.layers.length; i++) {
@@ -42,11 +20,76 @@ export function convert(source: string, destination: string) {
 
   style.layers = layers
 
-  try {
-    fs.writeFileSync(destinationPath, YAML.dump(style).replace(/'\!\!inc\/file layers\/.+\.yml'/g, function (match) {
-      return match.replace(/'/g, '')
-    }))
-  } catch(err) {
-    throw `${destinationPath}: Permission denied`
+  fs.writeFileSync(destinationPath, YAML.dump(style).replace(/'\!\!inc\/file layers\/.+\.yml'/g, function (match) {
+    return match.replace(/'/g, '')
+  }))
+}
+
+const getDestinationPath = (destination: string, sourcePath: string = '') => {
+  let destinationPath
+
+  if (destination) {
+    if (destination.match(/^\//)) {
+      destinationPath = destination
+    } else {
+      destinationPath = path.resolve(process.cwd(), destination)
+    }
+  } else {
+    if (sourcePath) {
+      destinationPath = path.join(path.dirname(sourcePath), `${path.basename(sourcePath, '.json')}.yml`)
+    } else {
+      destinationPath = path.join(process.cwd(), 'style.yml')
+    }
+  }
+
+  return destinationPath
+}
+
+export function convert(source: string, destination: string) {
+  let style, sourcePath
+
+  if ('-' === source) {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      terminal: false
+    });
+
+    const lines: string[] = []
+
+    rl.on("line", (line) => {
+      lines.push(line)
+    });
+
+    rl.on("close", () => {
+      const style = JSON.parse(lines.join(''))
+      const destinationPath = getDestinationPath(destination)
+
+      try {
+        writeYaml(destinationPath, style)
+      } catch(err) {
+        throw `${destinationPath}: Permission denied`
+      }
+    })
+  } else {
+    sourcePath = path.resolve(process.cwd(), source)
+
+    // The `source` is absolute path.
+    if (source.match(/^\//)) {
+      sourcePath = source
+    }
+
+    if (! fs.existsSync(sourcePath)) {
+      throw `${sourcePath}: No such file or directory`
+    }
+
+    style = JSON.parse(fs.readFileSync(sourcePath, 'utf-8'))
+
+    const destinationPath = getDestinationPath(destination, sourcePath)
+
+    try {
+      writeYaml(destinationPath, style)
+    } catch(err) {
+      throw `${destinationPath}: Permission denied`
+    }
   }
 }
