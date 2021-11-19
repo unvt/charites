54b16e3ee32636df52b530e53c0d232df23a41a0
Jin Igarashi <igarashi.jin@water-gis.com>
2021-11-19 09:30:03 +0000

    seperate cli.ts to multiple files for each command.

---
+++ src/cli/index.ts
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+import { Command } from 'commander';
+
+import init from './init'
+import convert from './convert'
+import build from './build'
+import serve from './serve'
+
+const program = new Command();
+
+program
+  .option('--provider [provider]', 'your map service. e.g. `mapbox`, `geolonia`')
+  .option('--mapbox-access-token [mapboxAccessToken]', 'Access Token for the Mapbox')
+  .addCommand(init)
+  .addCommand(convert)
+  .addCommand(build)
+  .addCommand(serve)
+
+program.parse(process.argv)
---
+++ src/cli/init.ts
@@ -0,0 +1,25 @@
+import { Command } from 'commander';
+import { init, initOptions } from '../commands/init'
+import { error } from './error'
+
+const program = new Command();
+program
+    .name('init')
+    .arguments('<file>')
+    .description('initialize a style JSON')
+    .option('-t, --tilejson-urls <tilejson_urls>', 'an URL for TileJSON. It will create empty layers from vector_layers property of TileJSON. Please use comma (,) in case multiple TileJSONs require.')
+    .option('-m, --metadatajson-urls <metadatajson_urls>', 'an URL for metadata.json. It will create empty layers from vector_layers property of metadata.json. Please use comma (,) in case multiple metadata.json require.')
+    .option('-c, --composite-layers', 'If it is true, a single YAML will be generated with multiple layers. Default is false.')
+    .action(async(file: string, initOptions: initOptions) => {
+        const options = program.opts()
+        options.tilejsonUrls = initOptions.tilejsonUrls
+        options.metadatajsonUrls = initOptions.metadatajsonUrls
+        options.compositeLayers = initOptions.compositeLayers
+        try {
+        await init(file, options)
+        } catch(e) {
+        error(e)
+        }
+    })
+
+export default program
