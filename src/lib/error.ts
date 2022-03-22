0479e511a1eb790c6d03750a59cfe6d0ec62f571
naoki <n.globe.us@gmail.com>
2022-03-22 11:49:43 +0900

    Fix to print error message

--- src/lib/error.ts
+++ src/lib/error.ts
@@ -1,6 +1,10 @@
 export const error = (e: unknown) => {
+
   if (e instanceof TypeError) {
     console.error(e.message)
+  } else if (typeof e === 'string') {
+    console.error(e)
   }
+
   process.exit(1)
 }
