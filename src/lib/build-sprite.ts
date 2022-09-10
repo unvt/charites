da54fe5ab7ca7a05474721c85f8ba21394190617
Taro Matsuzawa <btm@tech.email.ne.jp>
2022-09-10 13:57:04 +0900

    remove dependency for mapnik

--- src/lib/build-sprite.ts
+++ src/lib/build-sprite.ts
@@ -1,31 +1,5 @@
-const spritezero = require('@mapbox/spritezero')
-const fs = require('fs')
-const glob = require('glob')
+import { generateSprite } from '@smellman/sprite-one'
 const path = require('path')
-
-type spriteOption = {
-  imgs: string
-  pixelRatio: number
-  format: boolean
-}
-
-function generateLayoutAsync(option: spriteOption): Promise<string> {
-  return new Promise((resolve, reject) => {
-    spritezero.generateLayout(option, (err: TypeError, result: string) => {
-      if (err) reject(err)
-      else resolve(result)
-    })
-  })
-}
-
-function generateImageAsync(option: string): Promise<string> {
-  return new Promise((resolve, reject) => {
-    spritezero.generateImage(option, (err: TypeError, result: string) => {
-      if (err) reject(err)
-      else resolve(result)
-    })
-  })
-}
 
 export async function buildSprite(
   svgPath: string,
@@ -37,44 +11,16 @@ export async function buildSprite(
   for (let i = 0; i < pxRatios.length; i++) {
     const pxRatio = pxRatios[i]
 
-    const svgFiles = glob
-      .sync(path.join(svgPath, `*.svg`))
-      .map(function (iconPath: string) {
-        return {
-          svg: fs.readFileSync(iconPath),
-          id: path.basename(iconPath).replace('.svg', ''),
-        }
-      })
-
     let file = ''
     if (pxRatio > 1) {
       file = `@${pxRatio}x`
     }
 
-    const pngPath = path.join(publicPath, `${iconSlug}${file}.png`)
-    const jsonPath = path.join(publicPath, `${iconSlug}${file}.json`)
-
+    const outPath = path.join(publicPath, `${iconSlug}${file}`)
     try {
-      const dataLayout = await generateLayoutAsync({
-        imgs: svgFiles,
-        pixelRatio: pxRatio,
-        format: true,
-      })
-      fs.writeFileSync(jsonPath, JSON.stringify(dataLayout))
+      await generateSprite(outPath, svgPath, pxRatio)
     } catch (error) {
-      throw `${publicPath}: No such file or directory`
-    }
-
-    try {
-      const imageLayout = await generateLayoutAsync({
-        imgs: svgFiles,
-        pixelRatio: pxRatio,
-        format: false,
-      })
-      const image = await generateImageAsync(imageLayout)
-      fs.writeFileSync(pngPath, image)
-    } catch (error) {
-      throw `${publicPath}: No such file or directory`
+      throw error
     }
   }
 }
