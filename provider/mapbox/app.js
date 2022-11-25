ee1643dccaf5ff5284ef76a6a163ecb109aeebe6
yuiseki <yuiseki@gmail.com>
2022-11-25 09:57:46 +0900

    Show center of tiles when `charites serve` has launched (#119)
    * fix broken LegendControl of the `charites serve` command
    
    * Show center of tiles when `charites serve` has launched
    
    * Fix center and zoom of style json to take priority

--- provider/mapbox/app.js
+++ provider/mapbox/app.js
@@ -1,53 +1,91 @@
-mapboxgl.accessToken = '___MAPBOX_ACCESS_TOKEN___'
+;(async () => {
+  mapboxgl.accessToken = '___MAPBOX_ACCESS_TOKEN___'
 
-const map = new mapboxgl.Map({
-  container: 'map',
-  hash: true,
-  style: `http://${window.location.host}/style.json`,
-})
+  const mapStyleUrl = `http://${window.location.host}/style.json`
+  const mapStyleRes = await fetch(mapStyleUrl)
+  const mapStyleJson = await mapStyleRes.json()
 
-const socket = new WebSocket('ws://localhost:___PORT___')
+  // detect center & zoom from map style json
+  let center = mapStyleJson.hasOwnProperty('center')
+    ? mapStyleJson.center
+    : undefined
+  let zoom = mapStyleJson.hasOwnProperty('zoom') ? mapStyleJson.zoom : undefined
 
-socket.addEventListener('message', (message) => {
-  map.setStyle(JSON.parse(message.data))
-})
+  // detect center & zoom from tile json
+  if (center === undefined || zoom === undefined) {
+    for (const sourceName in mapStyleJson.sources) {
+      if (
+        mapStyleJson.sources[sourceName].type === 'vector' &&
+        mapStyleJson.sources[sourceName].hasOwnProperty('url')
+      ) {
+        const mapTileUrl = mapStyleJson.sources[sourceName].url
+        const mapTileRes = await fetch(mapTileUrl)
+        const mapTileJson = await mapTileRes.json()
+        if (center === undefined) {
+          const bounds = mapTileJson.bounds
+          center = mapTileJson.center
+            ? mapTileJson.center
+            : [(bounds[0] + bounds[2]) / 2, (bounds[1] + bounds[3]) / 2]
+        }
+        if (zoom === undefined) {
+          zoom = (mapTileJson.minzoom + mapTileJson.maxzoom) / 2
+        }
+      }
+    }
+  }
 
-map.addControl(new mapboxgl.NavigationControl(), 'top-right')
+  const map = new maplibregl.Map({
+    container: 'map',
+    hash: true,
+    style: mapStyleUrl,
+    center: center,
+    zoom: zoom + 2,
+  })
 
-map.addControl(
-  new MapboxLegendControl(
-    {},
-    {
-      showDefault: true,
-      showCheckbox: true,
-      onlyRendered: true,
-      reverseOrder: true,
-      accesstoken: mapboxgl.accessToken,
-    },
-  ),
-  'bottom-left',
-)
+  const socket = new WebSocket('ws://localhost:___PORT___')
 
-const showTileBoundaries = document.getElementById('showTileBoundaries')
-const setShowTileBoundaries = function () {
-  const checked = showTileBoundaries.checked
-  map.showTileBoundaries = checked
-}
-setShowTileBoundaries()
-showTileBoundaries.addEventListener('click', setShowTileBoundaries)
+  socket.addEventListener('message', (message) => {
+    map.setStyle(JSON.parse(message.data))
+  })
 
-const showCollisionBoxes = document.getElementById('showCollisionBoxes')
-const setShowCollisionBoxes = function () {
-  const checked = showCollisionBoxes.checked
-  map.showCollisionBoxes = checked
-}
-setShowCollisionBoxes()
-showCollisionBoxes.addEventListener('click', setShowCollisionBoxes)
+  map.addControl(new mapboxgl.NavigationControl(), 'top-right')
 
-const showPadding = document.getElementById('showPadding')
-const setShowPadding = function () {
-  const checked = showPadding.checked
-  map.showPadding = checked
-}
-setShowPadding()
-showPadding.addEventListener('click', setShowPadding)
+  map.addControl(
+    new MapboxLegendControl(
+      {},
+      {
+        showDefault: true,
+        showCheckbox: true,
+        onlyRendered: true,
+        reverseOrder: true,
+        accesstoken: mapboxgl.accessToken,
+      },
+    ),
+    'bottom-left',
+  )
+
+  const showTileBoundaries = document.getElementById('showTileBoundaries')
+  const setShowTileBoundaries = function () {
+    const checked = showTileBoundaries.checked
+    map.showTileBoundaries = checked
+  }
+  setShowTileBoundaries()
+  showTileBoundaries.addEventListener('click', setShowTileBoundaries)
+
+  const showCollisionBoxes = document.getElementById('showCollisionBoxes')
+  const setShowCollisionBoxes = function () {
+    const checked = showCollisionBoxes.checked
+    map.showCollisionBoxes = checked
+  }
+  setShowCollisionBoxes()
+  showCollisionBoxes.addEventListener('click', setShowCollisionBoxes)
+
+  const showPadding = document.getElementById('showPadding')
+  const setShowPadding = function () {
+    const checked = showPadding.checked
+    map.showPadding = checked
+  }
+  setShowPadding()
+  showPadding.addEventListener('click', setShowPadding)
+})()
+
