8d12ae29676386bf95513cc543f54a91eb4102f3
yuiseki <yuiseki@gmail.com>
2022-12-03 14:43:07 +0900

    Testing the live preview feature by playwright

---
+++ playwright.config.ts
@@ -0,0 +1,29 @@
+// playwright.config.ts
+import type { PlaywrightTestConfig } from '@playwright/test'
+
+const config: PlaywrightTestConfig = {
+  testDir: 'test/playwright',
+  use: {
+    browserName: 'chromium',
+    headless: true,
+  },
+  workers: 3,
+  webServer: [
+    {
+      command: 'dist/cli.js serve test/data/style.yml --port 8080',
+      port: 8080,
+    },
+    {
+      command:
+        'dist/cli.js serve test/data/style.yml --provider geolonia --port 8088',
+      port: 8088,
+    },
+    {
+      command:
+        'dist/cli.js serve test/data/style.yml --provider mapbox --port 8888 --mapbox-access-token ' +
+        process.env.mapboxAccessToken,
+      port: 8888,
+    },
+  ],
+}
+export default config
