683eca1976cbc971d1bfe7fbe6775babfe9ab0d8
yuiseki <yuiseki@gmail.com>
2022-12-15 10:19:25 +0900

    Add E2E Test of the live preview feature by Playwright (#131)
    * Testing the live preview feature by playwright
    * use secrets.MAPBOX_ACCESS_TOKEN
    * cache and restore playwright on github actions
    * use actions/cache@v3 instead of @v2

---
+++ test/playwright/provider/geolonia/e2e.spec.ts
@@ -0,0 +1,13 @@
+import { test, expect } from '@playwright/test'
+
+test('Charites Live Preview with geolonia', async ({ page }) => {
+  // collect errors on the page
+  const pageErrors: Error[] = []
+  page.on('pageerror', (exception) => pageErrors.push(exception))
+
+  await page.goto('http://localhost:8088/', { waitUntil: 'networkidle' })
+  await page.waitForTimeout(1000)
+  const title = await page.title()
+  expect(title).toBe('Charites Live Preview')
+  expect(pageErrors).toMatchObject([])
+})
